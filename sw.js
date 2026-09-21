e3c7d2fa5895dcc45059ff11a77a8875eb36a1ed
jdanielka-crypto <jdanielka@gmail.com>
2026-09-22 00:04:46 +0200

    Refresh cache for V2.7.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='essensplan-v275-1';
+const CACHE='essensplan-v276-1';
 const CORE=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
