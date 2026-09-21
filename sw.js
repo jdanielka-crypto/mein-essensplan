301e5fbb25bda4608ae3e7ca3d31ba2b3c32901d
jdanielka-crypto <jdanielka@gmail.com>
2026-09-21 23:00:41 +0200

    Offline-Unterstützung hinzufügen

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='essensplan-v23-1';
+const CORE=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));return}e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request))) });
